608ede8e3d9e8cec79e435a8d14a168e3f79eb5e
JB <jbkihhv@gmail.com>
2025-06-18 19:52:41 +0200

    Ajout modal /suppression galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,9 @@ export const allRoutes = [
    new Route("/", "Accueil", "/pages/home.html"),
    new Route("/galerie", "Galerie", "/pages/galerie.html"),
    new Route("/signin","connexion","/pages/signin.html"),
+   new Route("/signup","inscription","/pages/signup.html"),
+   new Route("/account","mon compte","/pages/account.html"),
+   new Route("/editPassword","changement de mot de passe","/pages/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
