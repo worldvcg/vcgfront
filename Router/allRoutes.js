d02a3de33411de776b986ae7e6e8190a2ceb5d9d
JB <jbkihhv@gmail.com>
2025-06-19 16:32:46 +0200

    gestion de l'affichage des réservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,7 @@ export const allRoutes = [
    new Route("/signup","inscription","/pages/signup.html"),
    new Route("/account","mon compte","/pages/account.html"),
    new Route("/editPassword","changement de mot de passe","/pages/editPassword.html"),
+   new Route("/allResa","Les réservations","/pages/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
