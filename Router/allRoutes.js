88888068a8802bf358b148f0dd446d3f4bef45fc
JB <jbkihhv@gmail.com>
2025-06-30 14:50:10 +0200

    route

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,14 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-   new Route("/", "Accueil", "/pages/home.html"),
-   new Route("/galerie", "Galerie", "/pages/galerie.html"),
-   new Route("/signin","connexion","/pages/signin.html","/js/auth/signin.js"),
-   new Route("/signup","inscription","/pages/signup.html","/js/auth/signup.js"),
-   new Route("/account","mon compte","/pages/account.html"),
-   new Route("/editPassword","changement de mot de passe","/pages/editPassword.html"),
-   new Route("/allResa","Les réservations","/pages/allResa.html"),
-   new Route("/reserver","Réserver","/pages/reserver.html"),
+   new Route("/", "Accueil", "/pages/home.html", []),
+   new Route("/galerie", "Galerie", "/pages/galerie.html", []),
+   new Route("/signin","connexion","/pages/signin.html", ["disconnected"],"/js/auth/signin.js"),
+   new Route("/signup","inscription","/pages/signup.html", ["disconnected"],"/js/auth/signup.js"),
+   new Route("/account","mon compte","/pages/account.html", ["client,admin"]),
+   new Route("/editPassword","changement de mot de passe","/pages/editPassword.html", ["clent,admin"]),
+   new Route("/allResa","Les réservations","/pages/allResa.html", ["client"]),
+   new Route("/reserver","Réserver","/pages/reserver.html", ["client"]),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
--- Router/router.js
+++ Router/router.js
@@ -2,7 +2,7 @@ import Route from "./Route.js";
 import { allRoutes, websiteName } from "./allRoutes.js";
 
 // Création d'une route pour la page 404 (page introuvable)
-const route404 = new Route("404", "Page introuvable", "/pages/404.html");
+const route404 = new Route("404", "Page introuvable", "/pages/404.html", []);
 
 // Fonction pour récupérer la route correspondant à une URL donnée
 const getRouteByUrl = (url) => {
@@ -26,6 +26,21 @@ const LoadContentPage = async () => {
   const path = window.location.pathname;
   // Récupération de l'URL actuelle
   const actualRoute = getRouteByUrl(path);
+  // Vérifier les droit d'acces à la page
+  const allRoleArray = actualRoute.authorize;
+  if(allRoleArray.length > 0){
+    if(allRoleArray.includes("disconnected")){
+      if(isConnected()){
+        window.location.replace("/");
+      }
+    }
+    else{
+      const roleUser = getRole();
+      if(!allRoleArray.includes(roleUser)){
+        window.location.replace("/");
+      }
+    }
+  }
   // Récupération du contenu HTML de la route
   const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
   // Ajout du contenu HTML à l'élément avec l'ID "main-page"
